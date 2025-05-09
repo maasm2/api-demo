9d7b1d46b8f6d5e90fa7c62fc756197c1cbbab59
maasm2 <maasm2@ulaval.ca>
2025-05-09 14:16:41 -0400

    connect to db

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const admin = require("firebase-admin");
+const credentials = require("./key.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials)
+})
+
+const db = admin.firestore();
+
+app.use(express.json());
+
+
+
 app.get('/', (req, res) => {
     res.send({
         message: 'API is working'
